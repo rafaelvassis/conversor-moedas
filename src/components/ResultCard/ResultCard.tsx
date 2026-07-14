890587dd216737fe3c23e8c6c1601f1c886bed4b
Rafael Vasconcelos <955502@sga.pucminas.br>
2026-07-14 13:59:29 -0300

    adiciona componente ResultCard com estilos correspondentes

--- src/components/ResultCard/ResultCard.tsx
+++ src/components/ResultCard/ResultCard.tsx
@@ -1,7 +1,20 @@
-import "./ResultCard.css"
+import "./ResultCard.css";
 
-export default function ResultCard(){
-    return (<div>
-        Resultado
-    </div>)
+export default function ResultCard() {
+  return (
+    <div className="card__container">
+      <div>
+        <p className="label__output">Valor original</p>
+        <p className="text__output">USD 2000.00</p>
+      </div>
+      <div>
+        <p className="label__output">Valor Convertido</p>
+        <p className="text__output">USD 2000.00</p>
+      </div>
+      <div>
+        <p className="label__output">Data</p>
+        <p className="text__output">xx/xx/xxxx</p>
+      </div>
+    </div>
+  );
 }
